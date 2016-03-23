41b32870e2436b8de1f5e5f800ff851400cfce52
nunu-e64 <nunu-e64@github.com>
2016-03-24 08:16:06 +0900

    create player

---
+++ mockone/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float first_speed = 10.0f;
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody> ();
+		rigid.velocity = new Vector3 (0, first_speed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
