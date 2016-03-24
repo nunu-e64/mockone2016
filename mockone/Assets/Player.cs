615e1679b51a3d8b593c1a5c3cff3aa047ad46f9
nunu-e64 <nunu-e64@github.com>
2016-03-24 10:34:48 +0900

    click and spin ball space

--- mockone/Assets/Player.cs
+++ mockone/Assets/Player.cs
@@ -4,15 +4,54 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	private float first_speed = 10.0f;
-	private Rigidbody rigid;
+	private Rigidbody playerRigidbody;
+	private GameObject playerCamera;
+	public GameObject star;
+	private GameObject activeStar;
+
+	private float power;
+	public float cameraSpeed;
 
 	// Use this for initialization
 	void Start () {
-		rigid = gameObject.GetComponent<Rigidbody> ();
-		rigid.velocity = new Vector3 (0, first_speed, 0);
+		playerRigidbody = this.GetComponent<Rigidbody> ();
+		playerRigidbody.velocity = new Vector3 (0, first_speed, 0);
+		playerCamera = this.GetComponentInChildren<Camera> ().gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			var mouseScreenPos = Input.mousePosition;
+			mouseScreenPos.z = 30.0f;
+			var mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+			activeStar = GameObject.Instantiate (star, mouseWorldPos, star.transform.localRotation) as GameObject;
+
+			playerCamera.transform.parent = null;
+			power = playerRigidbody.velocity.sqrMagnitude / ((transform.position - activeStar.transform.position).magnitude * playerRigidbody.mass);
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			playerCamera.transform.parent = this.transform;
+		}
+
+		if (Input.GetMouseButton (0)) {
+			playerRigidbody.AddForce ((activeStar.transform.position - transform.position).normalized * power);
+		} else {
+			var cameraLocalPosition = playerCamera.transform.localPosition;
+			var cameraLocalPositionZ = cameraLocalPosition.z;
+			cameraLocalPosition.z = 0;
+
+			if (cameraLocalPosition.magnitude > cameraSpeed) {
+				cameraLocalPosition -= (cameraLocalPosition).normalized * cameraSpeed;
+			} else if (cameraLocalPosition.magnitude > 0) {
+				cameraLocalPosition = Vector3.zero;
+			}
+
+			cameraLocalPosition.z = cameraLocalPositionZ;
+			playerCamera.transform.localPosition = cameraLocalPosition;
+		}
+			
+
 	}
 }
