74635e5513d0d78ad035f1ae10328f8cd2c15f8d
nunu-e64 <nunu-e64@github.com>
2016-04-12 02:02:57 +0900

    タップ制限範囲を左右に追加

--- mockone/Assets/Scripts/TouchManager.cs
+++ mockone/Assets/Scripts/TouchManager.cs
@@ -15,6 +15,7 @@ public class TouchManager : MonoBehaviour {
 	private const string TOUCH_OBJECT_TAG = "TouchObject";
 	private const float TOUCH_INTERVAL = 1.0f;
 	private float interval;
+	private const float UNTAPABLE_EDGE_WIDTH = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -40,7 +41,8 @@ public class TouchManager : MonoBehaviour {
 	}
 
 	void CreateGravitation (Vector3 _touchPos) {
-		if (interval > TOUCH_INTERVAL) {
+		var edge = -1 * Camera.main.ScreenToWorldPoint (Vector3.zero).x;
+		if (interval > TOUCH_INTERVAL && _touchPos.x > -edge + UNTAPABLE_EDGE_WIDTH && _touchPos.x < edge - UNTAPABLE_EDGE_WIDTH) {
 			GameObject obj = Instantiate (touchObject, _touchPos, Quaternion.identity) as GameObject;
 			obj.GetComponent<TouchObject> ().Init (touchObjectRadius);
 			movePlayer.GetComponent<MovePlayer> ().SetActionState (MovePlayer.ActionState.MOVE, obj);
