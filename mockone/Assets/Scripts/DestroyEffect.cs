57b404b5bc0f5705564a98e45e845811865953fc
宮里章太 <9miya20@gmail.com>
2016-04-28 01:41:42 +0900

    死亡時は死亡エフェクトの後にゲームオーバー画面

--- mockone/Assets/Scripts/DestroyEffect.cs
+++ mockone/Assets/Scripts/DestroyEffect.cs
@@ -4,10 +4,6 @@ using System.Collections;
 public class DestroyEffect : MonoBehaviour {
 	public bool isGameOver{ private get; set; }
 
-	void Start() {
-		isGameOver = false;
-	}
-
 	void AnimationFinish () {
 		if (isGameOver) CanvasManager.Instance.SetLogo (GameManager.GameState.GAME_OVER);
 		Destroy (this.gameObject);
