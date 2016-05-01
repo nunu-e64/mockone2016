502986b551c4e3b5df28b15cfc1e830d676c8310
宮里章太 <9miya20@gmail.com>
2016-05-01 23:49:37 +0900

    プロローグ修正

--- mockone/Assets/Scripts/PrologueManager.cs
+++ mockone/Assets/Scripts/PrologueManager.cs
@@ -9,22 +9,27 @@ public class PrologueManager : MonoBehaviour {
 	[SerializeField]
 	private Sprite[] sprites;
 
+	private float timeElapsed;
 	private int touchCount;
 
 	// Use this for initialization
 	void Start () {
+		this.timeElapsed = 0;
 		this.touchCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
+
+		timeElapsed += Time.deltaTime;
+		if (timeElapsed >= 2) {
 			this.touchCount ++;
 			if (touchCount == sprites.Length) {
 				GameManager.Instance.ChangeScene (GameManager.SceneName.StageSelect.ToString ());
 			} else {
 				image.GetComponent<Image> ().sprite = sprites[this.touchCount];
 			}
+			this.timeElapsed = 0;
 		}
 	}
 }
