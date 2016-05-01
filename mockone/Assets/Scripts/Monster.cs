2c906686eda23fdda4fab48e075562133d26b4cc
nunu-e64 <nunu-e64@github.com>
2016-05-02 02:18:54 +0900

    monster吹っ飛ばし方向の修正

--- mockone/Assets/Scripts/Monster.cs
+++ mockone/Assets/Scripts/Monster.cs
@@ -21,7 +21,7 @@ public class Monster : MonoBehaviour {
 
 	public virtual void Dead (Vector2 hitDirection) {
 		this.GetComponent<Animator> ().Stop ();
-		this.gameObject.GetComponent<Rigidbody2D>().velocity = Quaternion.Euler(0, 0, 30) * hitDirection * GameManager.Instance.MONSTER_BLAST_SPEED;
+		this.gameObject.GetComponent<Rigidbody2D>().velocity = Quaternion.Euler(0, 0, 0) * hitDirection * GameManager.Instance.MONSTER_BLAST_SPEED;
 		hasBlasted = true;
 	}
 }
