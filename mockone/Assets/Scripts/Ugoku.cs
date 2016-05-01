0f286c999ffd2b8e2173a741f1a51cfc6f7e769f
nunu-e64 <nunu-e64@github.com>
2016-05-02 04:09:47 +0900

    動く妖怪に無敵設定

--- mockone/Assets/Scripts/Ugoku.cs
+++ mockone/Assets/Scripts/Ugoku.cs
@@ -33,6 +33,8 @@ public class Ugoku : Monster {
 	private float t;
 	private Vector3 startPosition;
 	private bool isInvincible;
+	private bool hasPlayerStrongFinished;
+	private bool isInvincibleMoving;
 
 	// Use this for initialization
 	new void Start () {
@@ -42,38 +44,60 @@ public class Ugoku : Monster {
 		startPosition = this.transform.position;
 		this.changeMoveStyle(isFirst);
 		isInvincible = false;
+		hasPlayerStrongFinished = false;
 	}
 	
 	new void Update() {
 		base.Update();
 
-		this.changeMoveStyle (isFirst);		//DEBUG: for パラメータ調整しやすくするため毎回反映
-		if (!hasBlasted) {
-			t += Time.deltaTime;
-			float x = Mathf.Cos (t * speed / size) * size;
-			float y = Mathf.Sin (t * speed / size) * size;
-
-			switch (moveStyle) {
-			case MoveStyle.LEFT_CIRCLE:
-				this.transform.position = startPosition + new Vector3 (x, y, 0);
-				break;
-			case MoveStyle.RIGHT_CIRCLE:
-				this.transform.position = startPosition + new Vector3 (x, -y, 0);
-				break;
-			case MoveStyle.HORIZONTAL:
-				this.transform.position = startPosition + new Vector3 (x, 0, 0);
-				break;
-			case MoveStyle.VERTICAL:
-				this.transform.position = startPosition + new Vector3 (0, y, 0);
-				break;
-			case MoveStyle.NANAME_RIGHTTOP:
-				this.transform.position = startPosition + new Vector3 (x, x, 0);
-				break;
-			case MoveStyle.NANAME_LEFTTOP:
-				this.transform.position = startPosition + new Vector3 (x, -x, 0);
-				break;
-			}
+		//一度ダメージを受けてたら色を変える
+		if (this.isFirst) {
+			this.GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1, 1);
+		} else {
+			this.GetComponent<SpriteRenderer> ().color = new Color (255 / 255.0f, 170 / 255.0f, 70 / 255.0f, 1);
 		}
+
+		//無敵状態なら点滅
+		if (this.isInvincible) {
+			float alpha = (Mathf.Sin (Mathf.PI * t / 0.2f) + 1) / 2.0f;
+			this.GetComponent<SpriteRenderer> ().color = new Color(1, 1, 1, alpha);
+		}
+
+		this.changeMoveStyle (isFirst);		//DEBUG: for パラメータ調整しやすくするため毎回反映
+		if (!hasBlasted && !isInvincible) {
+			this.transform.position = startPosition + GetMoveDiff (this.t);
+		}
+
+		//無敵解除後に位置が飛ばないようにループの最後でインクリメント
+		t += Time.deltaTime;
+	}
+
+	private Vector3 GetMoveDiff(float t) {
+		float x = Mathf.Cos (t * speed / size) * size;
+		float y = Mathf.Sin (t * speed / size) * size;
+		Debug.Log (t);
+
+		switch (moveStyle) {
+		case MoveStyle.LEFT_CIRCLE:
+			return new Vector3 (x, y, 0);
+			break;
+		case MoveStyle.RIGHT_CIRCLE:
+			return new Vector3 (x, -y, 0);
+			break;
+		case MoveStyle.HORIZONTAL:
+			return new Vector3 (y, 0, 0);
+			break;
+		case MoveStyle.VERTICAL:
+			return new Vector3 (0, y, 0);
+			break;
+		case MoveStyle.NANAME_RIGHTTOP:
+			return new Vector3 (y, y, 0);
+			break;
+		case MoveStyle.NANAME_LEFTTOP:
+			return new Vector3 (y, -y, 0);
+			break;
+		}
+		return Vector3.zero;
 	}
 
 	public override void Dead(Vector2 hitDirection) {
@@ -81,6 +105,13 @@ public class Ugoku : Monster {
 			if (isFirst) {
 				this.changeMoveStyle (false);
 				isInvincible = true;
+				isInvincibleMoving = true;
+				Hashtable hash = new Hashtable ();
+				hash.Add ("oncompletetarget", this.gameObject);
+				hash.Add ("oncomplete", "FinishInvincibleMoving");
+				hash.Add ("time", 1.0f);
+				hash.Add ("position", startPosition + GetMoveDiff(0f));
+				iTween.MoveTo (this.gameObject, hash);
 			} else {
 				base.Dead (hitDirection);
 			}
@@ -101,6 +132,21 @@ public class Ugoku : Monster {
 	}
 
 	public override void FinishPlayerStrong() {
-		isInvincible = false;
+		if (isInvincible) {
+			hasPlayerStrongFinished = true;
+			if (!isInvincibleMoving) {
+				isInvincible = false;
+				t = 0.0f;
+			}
+		}
 	}
+
+	private void FinishInvincibleMoving() {
+		isInvincibleMoving = false;
+		if (hasPlayerStrongFinished) {
+			isInvincible = false;
+			t = 0.0f;
+		}
+	}
+						
 }
