eeeeea98df920eee8efb8ee61eb306392431e0ee
宮里章太 <9miya20@gmail.com>
2016-05-06 00:53:38 +0900

    タイトル、プロローグ、ステージセレクト修正

--- mockone/Assets/Scripts/Title.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Title : MonoBehaviour {
-
-	public void OnClick () {
-		GameManager.Instance.ChangeScene (GameManager.SceneName.Prologue.ToString ());
-	}
-}
