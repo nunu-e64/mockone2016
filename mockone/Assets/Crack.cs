c5497d8fe38fc8ea067a7982f1f43ef22881d54b
nunu-e64 <nunu-e64@github.com>
2016-05-08 07:50:26 +0900

    create crack

---
+++ mockone/Assets/Crack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crack : MonoBehaviour {
+
+	public int Hp = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
