eef3e0a7fa6a860b675a177d962ed0764871d4ab
nunu-e64 <nunu-e64@github.com>
2016-05-08 01:09:55 +0900

    add star image

--- mockone/Assets/Star.cs
+++ mockone/Assets/Star.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class Star : MonoBehaviour {
 
+	[SerializeField]
+	StarImageKind starImageKind;
+	[SerializeField]
+	private Sprite[] starImage;
 
-
-	// Use this for initialization
-	void Start () {
-	
+	private enum StarImageKind {
+		BLUE_BIG,
+		BLUE_SMALL,
+		RED_BIG,
+		RED_SMALL,
+		YELLOW_BIG,
+		YELLOW_SMALL
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Start () {
+		if (starImage.Length <= (int)starImageKind) {
+			Debug.LogError ("StarImage is less than starImageKindNum");
+		} else {
+			this.GetComponent<SpriteRenderer> ().sprite = starImage [(int)starImageKind];
+		}
 	}
 }
