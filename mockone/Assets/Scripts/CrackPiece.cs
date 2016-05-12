78532bc2ced6019860a15951b92c54949a9de4fa
Fumiya Amano <fumiya.amano@gree.co.jp>
2016-05-12 13:04:23 +0900

    set crackPieceChild Pos

--- mockone/Assets/Scripts/CrackPiece.cs
+++ mockone/Assets/Scripts/CrackPiece.cs
@@ -1,7 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CrackPiece : MonoBehaviour {
+
+	private List<GameObject> pieces;
+
+	void Start() {
+		pieces = new List<GameObject> ();
+
+		foreach (Transform child in transform) {
+			pieces.Add (child.gameObject);
+		}
+	}
+
+	void SprashPieces() {
+		foreach (var piece in pieces) {
+//			Vector2 dir = this.transform.position	
+			//iTween.mo
+		}
+	}
 
 	void AnimationFinish () {
 		Destroy (this.gameObject);
